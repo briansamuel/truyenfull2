6d646b2b3282278d5e42ad310432c11ee3f0e2f5
briansamuel <dinhvanvu94@gmail.com>
2017-02-28 16:47:26 +0700

    4:47 28/2/2017

--- local/resources/views/admin/sidebar.blade.php
+++ local/resources/views/admin/sidebar.blade.php
@@ -39,58 +39,37 @@
         <li class="treeview">
             <a href="#">
                 <i class="fa fa-files-o"></i>
-                <span>Bài viết</span>
+                <span>Tuyển tập truyện</span>
                 <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
                 </span>
             </a>
             <ul class="treeview-menu">
-                <li><a href="admin/bai-viet"><i class="fa fa-circle-o"></i>Danh sách bài viết</a></li>
-                <li><a href="admin/them-bai-viet"><i class="fa fa-circle-o"></i>Thêm bài viết</a></li>
-                <li><a href="admin/them-bai-viet"><i class="fa fa-circle-o"></i>Thêm danh mục</a></li>
+                <li><a href="admin/story?action=allstory"><i class="fa fa-circle-o"></i>Danh sách truyện</a></li>
+                <li><a href="admin/story?action=addstory"><i class="fa fa-circle-o"></i>Thêm truyện</a></li>
+                <li><a href="admin/chapter?action=allchapter"><i class="fa fa-circle-o"></i>Danh sách chương</a></li>
+                <li><a href="admin/chapter?action=addchapter"><i class="fa fa-circle-o"></i>Thêm chương</a></li>
+                <li><a href="admin/categoy?action=addcategory"><i class="fa fa-circle-o"></i>Thêm danh mục</a></li>
             </ul>
         </li>
-        
         <li>
             <a href="#">
-                <i class="fa fa-book"></i>
-                <span>Trang</span>
-                <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-                </span>
-            </a>
-            <ul class="treeview-menu">
-                <li><a href="admin/danh-muc"><i class="fa fa-circle-o"></i>Danh sách trang</a></li>
-                <li><a href="admin/them-danh-muc"><i class="fa fa-circle-o"></i>Tạo trang</a></li>
-            </ul>
-        </li>
-
-        <li>
-            <a href="#">
-                <i class="fa fa-book"></i>
-                <span>Danh mục</span>
-                <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-                </span>
-            </a>
-            <ul class="treeview-menu">
-                <li><a href="admin/danh-muc"><i class="fa fa-circle-o"></i>Danh sách danh mục</a></li>
-                <li><a href="admin/them-danh-muc"><i class="fa fa-circle-o"></i>Thêm danh mục</a></li>
-            </ul>
-        </li>
-
-        <li>
-            <a href="#">
-                <i class="fa fa-book"></i>
+                <i class="fa fa-sticky-note"></i>
                 <span>Plugins</span>
                 <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
                 </span>
             </a>
             <ul class="treeview-menu">
-                <li><a href="admin/danh-muc"><i class="fa fa-circle-o"></i>Danh sách plugin</a></li>
-                <li><a href="admin/them-danh-muc"><i class="fa fa-circle-o"></i>Thêm plugin mới</a></li>
+                <li><a href="admin/plugins"><i class="fa fa-circle-o"></i>Danh sách plugin</a></li>
+                <li><a href="admin/plugins?action=addplugin"><i class="fa fa-circle-o"></i>Thêm plugin mới</a></li>
             </ul>
+        </li>
+        <li>
+            <a href="admin/comments">
+                <i class="fa fa-commenting-o"></i>
+                <span>Comments</span>
+            </a>
         </li>
         <li>
             <a href="#">
@@ -107,17 +86,16 @@
         </li>
         <li>
             <a href="#">
-                <i class="fa fa-book"></i>
+                <i class="fa fa-cog"></i>
                 <span>Cài đặt</span>
                 <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
                 </span>
             </a>
             <ul class="treeview-menu">
-                <li><a href="admin/danh-muc"><i class="fa fa-circle-o"></i>Cài đặt chung</a></li>
-                <li><a href="admin/them-danh-muc"><i class="fa fa-circle-o"></i>Đường dẫn</a></li>
-                <li><a href="admin/them-danh-muc"><i class="fa fa-circle-o"></i>Thư viện ảnh</a></li>
-                <li><a href="admin/them-danh-muc"><i class="fa fa-circle-o"></i>Đường dẫn</a></li>
+                <li><a href="admin/setting?action=general"><i class="fa fa-circle-o"></i>Cài đặt chung</a></li>
+                <li><a href="admin/setting?action=permalink"><i class="fa fa-circle-o"></i>Đường dẫn</a></li>
+                <li><a href="admin/setting?action=gallery"><i class="fa fa-circle-o"></i>Thư viện ảnh</a></li>
             </ul>
         </li>
     </ul>
